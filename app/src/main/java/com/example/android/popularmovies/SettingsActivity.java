89f6c294eedb6189ed337230773d66e9e2b56b4e
Zukka <info@zukkadev.net>
2018-03-17 17:39:46 +0100

    Updated minSdkVersion to 16

--- app/src/main/java/com/example/android/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/SettingsActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 
 /**
  * Created by zukka on 17/03/18.
@@ -13,6 +15,18 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
 
+        switch(view.getId()) {
+            case R.id.radio_popular:
+                if (checked)
+                    break;
+            case R.id.radio_rating:
+                if (checked)
+                    break;
+        }
     }
 }
