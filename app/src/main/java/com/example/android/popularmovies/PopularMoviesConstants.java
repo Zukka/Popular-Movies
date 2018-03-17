493b8f4e48e25e36b58b31521886467a76d19fa0
Zukka <info@zukkadev.net>
2018-03-17 17:54:40 +0100

    Add app constants class

---
+++ app/src/main/java/com/example/android/popularmovies/PopularMoviesConstants.java
@@ -0,0 +1,13 @@
+package com.example.android.popularmovies;
+
+/**
+ * Created by zukka on 17/03/18.
+ */
+
+public class PopularMoviesConstants {
+    public static final String apiKEY = "YOUR_API_KEY";
+
+    public static final String baseURL = "http://image.tmdb.org/t/p/";
+    public static final String imageSizeURL = "w185";
+
+}
