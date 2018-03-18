f632cd1b826fea4d771c57b2690d0f7feb8973d5
Zukka <info@zukkadev.net>
2018-03-18 11:50:36 +0100

    Add movie db URL constants

--- app/src/main/java/com/example/android/popularmovies/PopularMoviesConstants.java
+++ app/src/main/java/com/example/android/popularmovies/PopularMoviesConstants.java
@@ -5,9 +5,14 @@ package com.example.android.popularmovies;
  */
 
 public class PopularMoviesConstants {
-    public static final String apiKEY = "YOUR_API_KEY";
 
-    public static final String baseURL = "http://image.tmdb.org/t/p/";
+    public static final String imageBaseURL = "http://image.tmdb.org/t/p/";
     public static final String imageSizeURL = "w185";
 
+    public static final String theMovieDbBAseURL = "http://api.themoviedb.org/3";
+    public static final String theMovieDbPopularEndPoint = "/movie/popular";
+    public static final String theMovieDbTopRateEndPoint = "/movie/top_rated";
+
+    public static final String theMovieDbApiKEYValue = "YOUR_API_KEY";
+    public static final String theMovieDbApiKEYParameter = "?api_key=";
 }
