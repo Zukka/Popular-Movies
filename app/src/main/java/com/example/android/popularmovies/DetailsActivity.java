8028d6719da19b03ecbb0156a8d39d1a0727df23
Zukka <info@zukkadev.net>
2018-06-13 23:05:11 +0200

    First final version

--- app/src/main/java/com/example/android/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailsActivity.java
@@ -3,8 +3,10 @@ package com.example.android.popularmovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.nio.channels.InterruptibleChannel;
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -18,5 +20,19 @@ public class DetailsActivity extends AppCompatActivity {
         String filmVoteAverage = detailsIntent.getStringExtra("FilmVoteAverage");
         String filmReleaseDate = detailsIntent.getStringExtra("FilmReleaseDate");
 
+        TextView _filmTitle = findViewById(R.id.detailFilmTitle);
+        _filmTitle.setText(filmTitle);
+
+        TextView _filmOverView = findViewById(R.id.detailFilmOverView);
+        _filmOverView.setText(filmOverView);
+
+        TextView _filmVoteAverage = findViewById(R.id.detailFilmAverage);
+        _filmVoteAverage.setText(String.format("%s %s", getString(R.string.average), filmVoteAverage));
+
+        TextView _filmReleaseDate = findViewById(R.id.detailFilmReleaseDate);
+        _filmReleaseDate.setText(String.format("%s %s", getString(R.string.releaseDate), filmReleaseDate));
+
+        ImageView _filmPoster = findViewById(R.id.detailFilmImage);
+        Picasso.with(this).load(filmPosterURL).into(_filmPoster);
     }
 }
